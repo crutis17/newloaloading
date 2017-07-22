e33b9c8e06d839c05f0049c99ef320bceec0b090
crutis17 <noahsimons21@gmail.com>
2017-07-22 14:10:11 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -51,7 +51,7 @@ Config.songShuffle = true;
 // "circle"
 //
 // default: "bars"
-Config.background = "quadbars";
+Config.background = "squares";
 
 // Background opacity
 // sets the opacity of the audio visualization
